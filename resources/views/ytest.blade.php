9a57bb83b4e0895abd4087ba4090a31365460cfd
lomt1k3y <yakushin_1945@bk.ru>
2022-11-11 18:44:57 +0300

    добавил шапку

---
+++ resources/views/ytest.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comics shop</title>
+    <link rel="stylesheet" href="yestyle.css">
+</head>
+<body>
+<div class="preview-back">
+    <div class="preview-list">
+        <div class="nav-bar">
+            <ul>
+                <li><a href="#">Главная</a></li>
+                <li><a href="#1">Наши локации </a></li>
+                <li><a href="#2">Акции</a></li>
+                <li><a href="#3">Отзывы</a></li>
+                <li><a href="#3">Забронировать</a></li>
+            </ul>
+        </div>
+        
+    </div> 
+</body>
+</html>
