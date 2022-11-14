84c188ec55268dbdff94d2a836d6d1e192ffece2
lomt1k3y <yakushin_1945@bk.ru>
2022-11-14 20:59:56 +0300

    обновил html и стили

--- resources/views/ytest.blade.php
+++ resources/views/ytest.blade.php
@@ -4,22 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comics shop</title>
-    <link rel="stylesheet" href="yestyle.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{URL:: asset('css/yestyle.css')}}">
 </head>
 <body>
-<div class="preview-back">
-    <div class="preview-list">
-        <div class="nav-bar">
-            <ul>
-                <li><a href="#">Главная</a></li>
-                <li><a href="#1">Наши локации </a></li>
-                <li><a href="#2">Акции</a></li>
-                <li><a href="#3">Отзывы</a></li>
-                <li><a href="#3">Забронировать</a></li>
-            </ul>
+    <div class="intro">
+        <div class="video">
+            <video class="video_media" src="" autoplay muted loop></video>
         </div>
-        
-    </div> 
+        <div class="intro_content">
+            <div class="container">
+                <h1 class="intro_title"> sdfsdfsf</h1>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
